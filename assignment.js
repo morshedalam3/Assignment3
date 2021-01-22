8236f4bf9fb00cbf736b67839592cbdcc40a2b42
morshedalam3 <morshedhasan1969@gmail.com>
2021-01-22 19:31:48 +0600

    somethind perfect

--- assignment.js
+++ assignment.js
@@ -46,15 +46,12 @@
 
     function megaFriend(listOfFriend) {
         var biggestChar = "";
-    
         listOfFriend.forEach(function(word) {
         if(word.length > biggestChar.length) {
             biggestChar = word;
         }
-        });
-    
+        })
         return biggestChar;
     }
-    
     var word = megaFriend(["Tamim", "Morshed", "Hasan", "Durjoy"]);
     console.log(word); 
--- megaFriend.js
+++ megaFriend.js
@@ -1,15 +1,29 @@
 
+            // function megaFriend(listOfFriend) {
+            //     var biggestChar = "";
+            
+            //     listOfFriend.forEach(function(word) {
+            //     if(word.length > biggestChar.length) {
+            //         biggestChar = word;
+            //     }
+            //     });
+            
+            //     return biggestChar;
+            // }
+            
+            // var word = megaFriend(["Tamim", "Morshed", "Hasan", "Durjoy"]);
+            // console.log(word); 
+           
+        
+        
             function megaFriend(listOfFriend) {
                 var biggestChar = "";
-            
                 listOfFriend.forEach(function(word) {
                 if(word.length > biggestChar.length) {
                     biggestChar = word;
                 }
-                });
-            
+                })
                 return biggestChar;
             }
-            
             var word = megaFriend(["Tamim", "Morshed", "Hasan", "Durjoy"]);
             console.log(word); 
